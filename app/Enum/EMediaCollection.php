13197ee5bed80cac56f449b4fdaafca3510921d8
arzaaqi <arzaaqi555@gmail.com>
2024-10-15 13:31:49 +0700

    feat: rest api

--- app/Enum/EMediaCollection.php
+++ app/Enum/EMediaCollection.php
@@ -5,4 +5,5 @@ namespace App\enum;
 enum EMediaCollection: string
 {
     case USER_RECIPE_THUMBNAIL = 'user_recipe_thumbnail';
+    case USER_PROFILE_THUMBNAIL = 'user_profile_thumbnail';
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthenticatorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -17,15 +16,16 @@ Route::group([
     Route::post('/user', [UserController::class, 'store']);
     Route::put('/user/{id}', [UserController::class, 'update']);
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
-    
-    
+
+
 
     Route::get('/serverrecipe', [ServerRecipeController::class, 'index']);
     Route::get('/serverrecipe/{id}', [ServerRecipeController::class, 'show']);
 
-    Route::get('/user/{id}/userrecipe', [UserRecipeController::class, 'index']);
-    Route::post('/user/{id}/userrecipe', [UserRecipeController::class, 'store']);
-    Route::get('/userrecipe/{id}', [UserRecipeController::class, 'show']);
+    Route::get('/userrecipe/{user_id}', [UserRecipeController::class, 'index']);
+    Route::post('/userrecipe', [UserRecipeController::class, 'store']);
+    Route::get('/userrecipe/{user_id}/{id}', [UserRecipeController::class, 'show']);
     Route::put('/userrecipe/{id}', [UserRecipeController::class, 'update']);
     Route::delete('/userrecipe/{id}', [UserRecipeController::class, 'destroy']);
 });
+
